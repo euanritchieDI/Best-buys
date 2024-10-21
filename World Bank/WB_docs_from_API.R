bef379b82f8d6d259a59a612be5b14fe40d6da7c
Euan Ritchie <euan.ritchie@devinit.org>
2024-10-21 11:57:43 +0100

    add WB

---
+++ World Bank/WB_docs_from_API.R
@@ -0,0 +1,126 @@
+library(openxlsx)
+library(readxl)
+library(WDI)
+library(httr)
+library(jsonlite)
+library(tictoc) # for measuring run time
+library(glue)
+library(data.table)
+library(tidyverse)
+library(readtext)
+library(janitor)
+
+#########################################################
+
+## THIS BIT GETS EDUCATION ID CODES. COULD DO THIS FROM API BUT QUICKER AND 
+## EASIER TO GET FROM DOWNLOADING PROJECT LIST FROM WB WEBSITE
+
+#wbnew = read_excel("C:/Users/euan.ritchie/Downloads/all (2).xls",skip=2)
+#wbnew$educ = 1*(grepl("ducation",wbnew$sector1))
+#wbnew$educ2 = 1*(grepl("ducation",wbnew$sector1))
+#wbnew$educ2[grepl("ducation",wbnew$sector2)] = 1
+#wbnew$educ2[grepl("ducation",wbnew$sector3)] = 1
+
+##... however some projects seem to have disappeared relative to last download...
+wb = read_excel("C:/git/Best-buys/World Bank/data/WB_projects_21_10_2024.xls",skip=2)
+wb$educ = 1*(grepl("ducation",wb$sector1))
+wb$educ2 = 1*(grepl("ducation",wb$sector1))
+wb$educ2[grepl("ducation",wb$sector2)] = 1
+wb$educ2[grepl("ducation",wb$sector3)] = 1
+
+educs = unique(c(wb$id[wb$educ==1]))
+educs2 = unique(c(wb$id[wb$educ2==1]))
+
+
+# educs from wb best buys code
+
+#educs = "P123456,P234567,P345678"
+stub="https://search.worldbank.org/api/v2/wds?format=json&includepublicdocs=1&fl=*&os=0&rows=500&proid="
+
+tic()
+lnk = paste0(stub,educs2[1])
+req     = GET(url = lnk)
+res     = content(req, as = "text", encoding = "UTF-8")
+json    = fromJSON(res, flatten = TRUE)
+docjson = json
+
+for (i in educs2[-1]){
+	lnk = paste0(stub,i)
+	req     = GET(url = lnk)
+	res     = content(req, as = "text", encoding = "UTF-8")
+	json    = fromJSON(res, flatten = TRUE)
+	docjson = Map(c,docjson,json)
+}		
+toc()
+
+doclist = names(docjson$documents)
+doclist = unique(doclist[doclist!="facets"])
+
+sel = c("docty","docdt","display_title","projectid","txturl","pdfurl","repnb","prdln",
+"projn","id")
+
+docs = list()
+for (i in doclist){
+	jsonex = eval(parse(text=paste0("docjson$documents$",i)))
+	docs[[i]] = jsonex[sel]
+}
+
+dats = as.data.frame(rbindlist(docs,fill=T))
+
+pads = dats[dats$docty=="Project Appraisal Document",]
+pads = pads[!is.na(pads$projectid),]
+pads = remove_empty(pads, which = c("cols", "rows"))
+
+tic()
+cnt = 0
+padtext  =NULL
+for (i in pads$txturl){
+	toadd = tryCatch(readtext(i,encoding="Windows-1252")[,2],error=function(e) "fail")
+	padtext = c(padtext,toadd)
+	cnt = cnt+1
+	if(cnt%%20==0){print(cnt)}
+}
+toc()
+
+#-------------------------------------------------
+#STRING FUNCTIONS
+remove_punct = function(string){
+  str_replace_all(string, "[[:punct:]]", " ")
+}
+collapse_whitespace = function(string){
+  str_replace_all(string, "\\s+", " ")
+}
+#-------------------------------------------------
+
+padtext = remove_punct(collapse_whitespace(padtext))
+padtext = tolower(padtext)
+
+preprim = str_count(padtext,"pre[- ]?primary")
+tarl    = str_count(padtext,"teaching ?at ?the ?right ?level|\\btarl\\b")
+stped   = str_count(padtext,"structured ?pedagogy")
+
+pads$preprim = preprim
+pads$tarl    = tarl
+pads$stped   = stped
+pads$docyear = year(pads$docdt)
+pads$totalchar = nchar(padtext)
+pads$display_title = gsub("\\s{2,}"," ",pads$display_title)
+pads$mainsec = 1*(pads$projectid %in% educs)
+pads$anyBB = with(pads,1*(preprim>0 | stped>0 | tarl>0))
+pads$anyBB_nopreprim = with(pads,1*(stped>0 | tarl>0))
+
+pads = left_join(pads,wb[c("id","boardapprovaldate","curr_total_commitment","sector1")],by="id")
+
+write.csv(pads,"C:/git/Best-buys/World Bank/data/WorldBank_Education_projects.csv",row.names=F)
+as.data.frame(pads %>% group_by(docyear) %>% summarize(mean(stped)))
+
+with(pads,table(docyear,tarl))
+
+
+justrelevant = pads %>% filter(tarl>0 | preprim>0 | stped>0)
+
+
+
+
+
+
